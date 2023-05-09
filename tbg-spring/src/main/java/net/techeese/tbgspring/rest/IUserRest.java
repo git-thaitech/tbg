d40107b9e887a8a50c2686ab4a7fb2f27c8ea422
dev1 <dev1@techeese.net>
2023-05-09 22:35:55 +0700

    Update Auth

--- tbg-spring/src/main/java/net/techeese/tbgspring/rest/IUserRest.java
+++ tbg-spring/src/main/java/net/techeese/tbgspring/rest/IUserRest.java
@@ -1,7 +1,14 @@
 package net.techeese.tbgspring.rest;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Map;
 
 @RequestMapping(path = "/user")
 public interface IUserRest {
+    @PostMapping(path = "/signup")
+    public ResponseEntity<String> signUp(@RequestBody(required = true) Map<String, String> requestMapping);
 }
--- tbg-spring/src/main/java/net/techeese/tbgspring/rest/Impl/UserRestImpl.java
+++ tbg-spring/src/main/java/net/techeese/tbgspring/rest/Impl/UserRestImpl.java
@@ -1,8 +1,20 @@
 package net.techeese.tbgspring.rest.Impl;
 
 import net.techeese.tbgspring.rest.IUserRest;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 public class UserRestImpl implements IUserRest {
+    @Override
+    public ResponseEntity<String> signUp(Map<String, String> requestMapping) {
+        try {
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
